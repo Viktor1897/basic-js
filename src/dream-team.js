5ef772ae1ef063b08f96183cc35037004ba86ecb
Viktor Kolosov <vitykolosov@gmail.com>
2021-02-25 00:37:30 +0300

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  teamName = '';
+  if (!Array.isArray(members)) return false;
+  for (let member of members) {
+    if (typeof(member)=='string') {
+      member = member.trim();
+      teamName += (member[0]);
+    }
+  }
+  return teamName.toUpperCase().split('').sort().join('');
 };
