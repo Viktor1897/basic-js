a15ab6b134c32636c8491c962ca1ca12afaeb2d7
Viktor Kolosov <vitykolosov@gmail.com>
2021-03-01 19:37:40 +0300

    fix: update carbon-dating task and tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,10 +4,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if ((typeof(sampleActivity)=='string') && (+sampleActivity>1) && (+sampleActivity<15)) {
-    return Math.ceil((Math.log(MODERN_ACTIVITY/+sampleActivity))/(Math.LN2/HALF_LIFE_PERIOD));
+  if ((typeof(sampleActivity)=='string') && (+sampleActivity>0) && (+sampleActivity<15)) {
+    return Math.ceil((Math.log(MODERN_ACTIVITY/+sampleActivity))/(0.693/HALF_LIFE_PERIOD));
   } else {
     return false;
   }
-  
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -10,7 +10,6 @@ const chainMaker = {
     this.chain.push(`( ${value} )~~`);
     return this;
   },
-    
   removeLink(position) {
     if (position<1 || position>this.chain.length-1) {
       this.chain = [];
