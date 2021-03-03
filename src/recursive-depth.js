7596e10eb1f4b32413ba1710e31d767974c96bd1
Viktor Kolosov <vitykolosov@gmail.com>
2021-03-03 23:36:25 +0300

    feat: complete extended-repeater task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-//[1, 2, 3, 4, 5, [1]]
+  
   calculateDepth(arr, maxDepth = 1) {
     for (let i = 0; i<arr.length; i++) {
       if (Array.isArray(arr[i])) {
